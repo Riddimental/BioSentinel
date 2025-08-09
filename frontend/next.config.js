c3ab31e93095a978100d0cb82e467370de9f54a1
tsisasDaguzman <daguzman@tsitecnologia.com.co>
2025-08-09 12:15:13 -0500

    config: deploy vercel

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // Ignorar ESLint en el build de producción
-    ignoreDuringBuilds: true,
-  },
-  // silencia TS en build
-  typescript: { ignoreBuildErrors: true },
+  // eslint: {
+  //   // Ignorar ESLint en el build de producción
+  //   ignoreDuringBuilds: true,
+  // },
+  // // silencia TS en build
+  // typescript: { ignoreBuildErrors: true },
 };
 
 module.exports = nextConfig;
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // silencia ESLint en build
-  eslint: { ignoreDuringBuilds: true },  
-  // silencia TS en build
-  typescript: { ignoreBuildErrors: true },
+  // // silencia ESLint en build
+  // eslint: { ignoreDuringBuilds: true },  
+  // // silencia TS en build
+  // typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
