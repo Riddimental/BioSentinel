f3218b2bb6c8f3584a4bd3c1193194eb600e74ab
Daga2001 <dav1dguxman@hotmail.com>
2025-08-10 12:40:27 -0500

    fix: domain deploy vercel

--- frontend/app/environment.ts
+++ frontend/app/environment.ts
@@ -1,2 +1,2 @@
 // export const API_BASE_URL = 'http://127.0.0.1:8000';
-export const API_BASE_URL = 'https://silver-eureka-5r677r5g9wwcv5rw-8000.app.github.dev';
+export const API_BASE_URL = 'https://biosentinel-uv-1052563878931.us-east1.run.app';
