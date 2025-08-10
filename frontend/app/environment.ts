246a445ce902b7ef97e9aea16822c80d1c07230c
Daga2001 <dav1dguxman@hotmail.com>
2025-08-10 11:04:42 -0500

    fix: domain deploy vercel

--- frontend/app/environment.ts
+++ frontend/app/environment.ts
@@ -1,1 +1,2 @@
-export const API_BASE_URL = 'http://127.0.0.1:8000';
+// export const API_BASE_URL = 'http://127.0.0.1:8000';
+export const API_BASE_URL = 'https://silver-eureka-5r677r5g9wwcv5rw-8000.app.github.dev';
